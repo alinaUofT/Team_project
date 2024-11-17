60a1d1a5a9935ffd5729299a50368db3baa1fdb2
Alina Tiemiertiei <alina.tiemiertiei@mail.utoronto.ca>
2024-11-17 15:18:13 -0500

    Made code run

--- src/main/java/interface_adapter/search_results/SearchResultsViewModel.java
+++ src/main/java/interface_adapter/search_results/SearchResultsViewModel.java
@@ -6,15 +6,15 @@ import interface_adapter.movie.MovieState;
 /**
  * The View Model for the Movie View.
  */
-public class SearchResultsViewModel extends ViewModel<MovieState>  {
-    public static final String TITLE_LABEL = "Search Results";
-    public static final String HOME_LABEL = "Home";
-    public static final String BACK_LABEL = "Back";
-    public static final String SEARCH_LABEL = "Search";
-    public static final String ENTER_LABEL = "Enter";
-
-    public SearchResultsViewModel(String viewName) {
-        super("search results");
-        setState(new SearchResultsState());
-    }
-}
+//public class SearchResultsViewModel extends ViewModel<MovieState>  {
+//    public static final String TITLE_LABEL = "Search Results";
+//    public static final String HOME_LABEL = "Home";
+//    public static final String BACK_LABEL = "Back";
+//    public static final String SEARCH_LABEL = "Search";
+//    public static final String ENTER_LABEL = "Enter";
+//
+//    public SearchResultsViewModel(String viewName) {
+//        super("search results");
+//        setState(new SearchResultsState());
+//    }
+//}
