ed3cb65f678f270d3a8a885707e4ac32524e9c70
wyattanderson <wyatt.anderson@mail.utoronto.ca>
2024-11-28 20:44:10 -0500

    Fixed more checkstyle errors.

--- src/main/java/data_access/DBUserDataAccessObject.java
+++ src/main/java/data_access/DBUserDataAccessObject.java
@@ -1,19 +1,14 @@
 package data_access;
 
-import static com.mongodb.client.model.Filters.eq;
-
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
-import entity.*;
-
 import static com.mongodb.client.model.Filters.eq;
-
-import com.mongodb.client.FindIterable;
 import com.mongodb.client.MongoCollection;
 import org.bson.Document;
-
+import com.mongodb.client.FindIterable;
+import entity.*;
 import use_case.create_watchlist.CreateWatchlistDataAccessInterface;
 import use_case.home.HomeUserDataAccessInterface;
 import use_case.login.LoginUserDataAccessInterface;
@@ -21,9 +16,9 @@ import use_case.logout.LogoutUserDataAccessInterface;
 import use_case.my_reviews.MyReviewsDataAccessInterface;
 import use_case.recommendations.RecommendationsUserDataAccessInterface;
 import use_case.signup.SignupUserDataAccessInterface;
+import use_case.survey1.Survey1UserDataAccessInterface;
 import use_case.survey_second_page.SurveySecondPageDataAccessInterface;
 import use_case.watchlist.WatchlistUserDataAccessInterface;
-import use_case.survey1.Survey1UserDataAccessInterface;
 import use_case.watchlists.WatchlistsUserDataAccessInterface;
 
 /**
@@ -76,8 +71,8 @@ public class DBUserDataAccessObject implements SignupUserDataAccessInterface,
 
     @Override
     public boolean existsByName(String username) {
-        MongoCollection<Document> collection = DataBaseConstructor.GetCollection("Users");
-        FindIterable<Document> findIterable = collection.find(eq("userId", username));
+        final MongoCollection<Document> collection = DataBaseConstructor.GetCollection("Users");
+        final FindIterable<Document> findIterable = collection.find(eq("userId", username));
 
         return findIterable.first() != null;
     }
@@ -89,22 +84,18 @@ public class DBUserDataAccessObject implements SignupUserDataAccessInterface,
 
     @Override
     public void save(User user) {
-        Document newAccount = new Document("userId", user.getName())
+        final Document newAccount = new Document("userId", user.getName())
                 .append("username", user.getName())
                 .append("password", user.getPassword());
-        try {
-            collection.insertOne(newAccount);
-            System.out.println("User saved successfully");
-        } catch (Exception e) {
-            System.out.println("User not saved: " + e.getMessage());
-        }
+        collection.insertOne(newAccount);
+
     }
 
     @Override
     public boolean saveWatchlist(User user, Watchlist watchlist) {
         try {
             // Create a document representing the review
-            Document watchlistDoc = new Document()
+            final Document watchlistDoc = new Document()
                     .append("watchlistName", watchlist.getListName());
 
             collection.updateOne(
@@ -119,42 +110,41 @@ public class DBUserDataAccessObject implements SignupUserDataAccessInterface,
         }
     }
 
+    /**
+     * Leave a review for this user in the online DB.
+     * @param review the review object to add to this user.
+     * @return
+     */
     public boolean leaveReview(MovieReview review) {
-        try {
-            // Create a document representing the review
-            Document reviewDoc = new Document()
+        final Document reviewDoc = new Document()
                     .append("movieTitle", review.getMovie_Title())
                     .append("date", review.getDate())
                     .append("starRating", review.getStarRating());
 
-            // Add optional written review if provided
-            if (review.getContent() != null) {
-                reviewDoc.append("writtenReview", review.getContent());
-            }
-
-            // Add the review to the "reviews" array in the user's document
-            collection.updateOne(
-                    new Document("userId", review.getUserID()), // Find user by ID
-                    new Document("$push", new Document("reviews", reviewDoc)) // Push the new review
-            );
-
-            return true; // Indicate success
-        } catch (Exception e) {
-            System.err.println("Error adding review to user: " + e.getMessage());
-            return false; // Indicate failure
+        if (review.getContent() != null) {
+            reviewDoc.append("writtenReview", review.getContent());
         }
+
+        collection.updateOne(
+                    new Document("userId", review.getUserID()),
+                    new Document("$push", new Document("reviews", reviewDoc))
+        );
+
+        return true;
     }
 
-
+    /**
+     * Retrieve the reviews for this user.
+     * @param user a user of this program.
+     * @return
+     */
     public List<MovieReview> getReviews(User user) {
-        // Initialize the factory to create MovieReview objects
+
         final CommonMovieReviewFactory reviewFactory = new CommonMovieReviewFactory();
 
-        // Prepare the list to hold the user's reviews
         final List<MovieReview> reviews = new ArrayList<>();
 
-        // Query the "Users" collection to find the user and their reviews
-        Document userDoc = collection.find(new Document("userId", user.getName())).first();
+        final Document userDoc = collection.find(new Document("userId", user.getName())).first();
 
         if (userDoc != null) {
             // Extract the user's reviews (assuming reviews are stored in a sub-document or array)
@@ -170,55 +160,19 @@ public class DBUserDataAccessObject implements SignupUserDataAccessInterface,
                     final String movieTitle = reviewDoc.getString("movieTitle");
 
                     // Use the factory to create the MovieReview
-                    MovieReview review;
+                    final MovieReview review;
                     if (writtenReview != null) {
                         review = reviewFactory.create(username, date, starRating, writtenReview, movieTitle);
                     } else {
                         review = reviewFactory.create(username, date, starRating, movieTitle);
                     }
-
-                    // Add the review to the list
                     reviews.add(review);
                 }
             }
         }
 
-        // Return the list of reviews
         return reviews;
     }
-
-//    @Override
-//    public void changePassword(User user) {
-//        final OkHttpClient client = new OkHttpClient().newBuilder()
-//                .build();
-//
-//        // POST METHOD
-//        final MediaType mediaType = MediaType.parse(CONTENT_TYPE_JSON);
-//        final JSONObject requestBody = new JSONObject();
-//        requestBody.put(USERNAME, user.getName());
-//        requestBody.put(PASSWORD, user.getPassword());
-//        final RequestBody body = RequestBody.create(requestBody.toString(), mediaType);
-//        final Request request = new Request.Builder()
-//                .url("http://vm003.teach.cs.toronto.edu:20112/user")
-//                .method("PUT", body)
-//                .addHeader(CONTENT_TYPE_LABEL, CONTENT_TYPE_JSON)
-//                .build();
-//        try {
-//            final Response response = client.newCall(request).execute();
-//
-//            final JSONObject responseBody = new JSONObject(response.body().string());
-//
-//            if (responseBody.getInt(STATUS_CODE_LABEL) == SUCCESS_CODE) {
-//                // success!
-//            }
-//            else {
-//                throw new RuntimeException(responseBody.getString(MESSAGE));
-//            }
-//        }
-//        catch (IOException | JSONException ex) {
-//            throw new RuntimeException(ex);
-//        }
-//    }
 
     @Override
     public String getCurrentUsername() {
